195ee970184477d84780d9ae5aa61beb94ff9c81
soporteclientesanonimos <developervilchez@gmail.com>
2020-10-20 22:13:30 -0500

    agrega file keys

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,13 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./keys');
 
 passport.use(
   new GoogleStrategy({
     //opciones para la estrategia
-    clientID : "#",
-    clientSecret : "#",
+    clientID : keys.google.clientID,
+    clientSecret : keys.google.clientSecret,
     callbackURL : "#"
-  }), () => {
+  }, () => {
     //passport callback function
-  }
-)
+  }))
